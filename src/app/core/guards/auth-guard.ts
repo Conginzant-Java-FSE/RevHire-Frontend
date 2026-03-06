4915fc816d5d820649aa8cf88cc97982ac3d3344
Krishnakishore28 <krishposina88@gmail.com>
2026-03-06 05:38:53 +0530

    Pushing Guards Feature

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,5 +1,28 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isAuthenticated()) {
+    const requiredRole = route.data['role'];
+    const blockedRoles = route.data['blockedRoles'];
+
+    if (requiredRole && authService.currentUser()?.role !== requiredRole) {
+      router.navigate(['/']); 
+      return false;
+    }
+
+    if (blockedRoles && blockedRoles.includes(authService.currentUser()?.role)) {
+      router.navigate(['/employer/dashboard']); 
+      return false;
+    }
+
+    return true;
+  }
+
+  router.navigate(['/auth/login'], { queryParams: { returnUrl: state.url } });
+  return false;
 };
