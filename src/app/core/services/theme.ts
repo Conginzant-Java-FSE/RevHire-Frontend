d5b09b92e21503c2e42f7c89a7f6a122fae57de9
tejamahindra <tejamahindramalleswarapu@gmail.com>
2026-03-06 05:46:41 +0530

    added core theme

--- src/app/core/services/theme.ts
+++ src/app/core/services/theme.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Theme {
-  
+export class ThemeService {
+  isDarkMode = signal<boolean>(false);
+
+  constructor() {
+    this.initTheme();
+  }
+
+  private initTheme() {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme === 'dark' || (!storedTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      this.setDarkMode(true);
+    } else {
+      this.setDarkMode(false);
+    }
+  }
+
+  toggleTheme() {
+    this.setDarkMode(!this.isDarkMode());
+  }
+
+  private setDarkMode(isDark: boolean) {
+    this.isDarkMode.set(isDark);
+    if (isDark) {
+      document.documentElement.setAttribute('data-bs-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-bs-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 }
